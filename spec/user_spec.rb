e009a1ebc02f5d388cc18d5953dee02afb7c5671
Dylan Palmboom <nofxboy1234@gmail.com>
2023-05-02 08:15:29 +0200

    Upate tests

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -21,15 +21,32 @@ class User
 end
 
 describe User do
+  context 'when born in 2001' do
+    it 'is not born in a leap year' do
+      user = User.new('Francisca', '2001-01-01')
+      expect(user).not_to be_born_in_leap_year
+    end
+  end
+
+  context 'when born in 1900' do
+    it 'is not born in a leap year' do
+      user = User.new('Francisca', '1900-01-01')
+      expect(user).not_to be_born_in_leap_year
+    end
+  end
+
   context 'when born in 2000' do
     it 'is born in a leap year' do
       user = User.new('Francisca', '2000-01-01')
-      # expect(user.born_in_leap_year?).to eq true
       expect(user).to be_born_in_leap_year
     end
-
-    # example 'is at voting age' do
-      
-    # end
   end
+
+  context 'when born in 2004' do
+    it 'is born in a leap year' do
+      user = User.new('Francisca', '2004-01-01')
+      expect(user).to be_born_in_leap_year
+    end
+  end
+
 end

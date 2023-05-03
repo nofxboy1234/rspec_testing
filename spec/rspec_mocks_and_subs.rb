3086716e0bd27025f69e45cbef8426c53c7c32fc
Dylan Palmboom <nofxboy1234@gmail.com>
2023-05-03 09:26:52 +0200

    Add payment_gateway Stub

--- spec/rspec_mocks_and_subs.rb
+++ spec/rspec_mocks_and_subs.rb
@@ -46,7 +46,10 @@ end
 
 describe Payment do
   it 'records the payment' do
-    payment_gateway = PaymentGateway.new
+    # payment_gateway = PaymentGateway.new
+    payment_gateway = double
+    allow(payment_gateway).to receive(:charge).and_return(payment_id: 1234)
+
     logger = Logger.new
 
     payment = Payment.new(payment_gateway, logger)

04588fd21a3464ec081c46046e0796ff4554ecea
Dylan Palmboom <nofxboy1234@gmail.com>
2023-05-03 16:02:45 +0200

    Get #url_for test to pass

--- lib/router.rb
+++ lib/router.rb
@@ -1,3 +1,14 @@
 class Router
+  def initialize(domain:, protocol: 'https')
+    @domain = domain
+    @protocol = protocol
+  end
   
+  def url_for(object)
+    "#{@protocol}://#{object.blog.subdomain}.#{@domain}/#{object.slug}"
+  end
+
+  private
+
+  attr_reader :domain, :protocol
 end
--- spec/router_spec.rb
+++ spec/router_spec.rb
@@ -1,5 +1,19 @@
 require './lib/router'
+require './lib/blog'
+require './lib/post'
 
 RSpec.describe Router do
-  subject { described_class.new }
+  subject { described_class.new(domain: 'bloggo.com') }
+
+  describe '#url_for' do
+    it 'returns the full URL with protocol, subdomain, and path' do
+      blog = Blog.new(title: 'Brett Codes', subdomain: 'brettcodes')
+      post = Post.new(title: 'Using RSpec Test Doubles',
+                      content: 'Some stuff', blog: blog)
+      
+      expect(subject.url_for(post)).to eql(
+        'https://brettcodes.bloggo.com/using-rspec-test-doubles'
+      )
+    end
+  end
 end

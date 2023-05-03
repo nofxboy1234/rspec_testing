4d835d4d226beb7f88701a0d1f0580826593b0b9
Dylan Palmboom <nofxboy1234@gmail.com>
2023-05-03 13:42:31 +0200

    Add hunger level to Dog

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,13 @@
 class Dog
+  def initialize(hunger_level: 0)
+
+  end
+  
   def bark
     'Woof!'
+  end
+
+  def hungry?
+    true
   end
 end
--- spec/dog_spec.rb
+++ spec/dog_spec.rb
@@ -1,10 +1,18 @@
 require './lib/dog'
 # require_relative '../lib/dog'
+require 'pry-byebug'
 
 describe Dog do
   describe '#bark' do
     it 'returns "Woof!"' do
       expect(subject.bark).to eql('Woof!')
+    end
+  end
+
+  describe '#hungry?' do
+    it 'returns true if hunger_level is more than 5' do
+      hungry_dog = Dog.new(hunger_level: 7)
+      expect(hungry_dog.hungry?).to eq(true)
     end
   end
 end

3c57e0eb56a31132eb88a11242aed4060eaa9a4f
Dylan Palmboom <nofxboy1234@gmail.com>
2023-05-03 14:43:46 +0200

    Test #feed

--- spec/dog_spec.rb
+++ spec/dog_spec.rb
@@ -9,6 +9,17 @@ RSpec.describe Dog do
     end
   end
 
+  describe '#feed' do
+    context 'when the dog is hungry' do
+      subject { described_class.new(hunger_level: 7) }
+  
+      it 'is no longer hungry' do
+        subject.feed
+        expect(subject).to_not be_hungry
+      end
+    end
+  end
+
   describe '#hungry?' do
     context 'when hunger_level is more than 5' do
       subject { described_class.new(hunger_level: 7) }
@@ -17,7 +28,6 @@ RSpec.describe Dog do
         expect(subject).to be_hungry
       end
     end
-
     context 'when hunger_level is 5 or less' do
       subject { described_class.new(hunger_level: 5) }
 

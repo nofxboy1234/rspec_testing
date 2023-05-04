4eed07426c9e3024666a385080a0bf361c4063f9
Dylan Palmboom <nofxboy1234@gmail.com>
2023-05-04 15:35:42 +0200

    Update stub/mock info

--- spec/rspec_mocks_and_subs.rb
+++ spec/rspec_mocks_and_subs.rb
@@ -57,13 +57,15 @@ end
 describe Payment do
   it 'records the payment' do
     payment_gateway = double # RSpec::Mocks::Double (Stub)
-    # A stub is only a method with a canned response, it doesn’t care about behavior.
+    # A stub is only a method with a canned response, it doesn’t care 
+    # about behavior.
     allow(payment_gateway).to receive(:charge).and_return(payment_id: 1234)
 
     logger = double # RSpec::Mocks::Double (Mock)
     # It needs (not just can, but has to, and it will raise an exception if not)
     # to receive a record_payment method call with the value 1234
-    # A mock expects methods to be called, if they are not called the test will fail.
+    # A mock expects methods to be called, if they are not called the test will 
+    # fail.
 
     # You use mocks to test the interaction between two objects. 
     # Instead of testing the output value, like in a regular expectation.

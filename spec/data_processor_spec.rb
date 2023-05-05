3ccee5a03eda190a32e0f091e75fb9f523f4a86d
Dylan Palmboom <nofxboy1234@gmail.com>
2023-05-05 10:42:07 +0200

    Test for invalid data

--- spec/data_processor_spec.rb
+++ spec/data_processor_spec.rb
@@ -1,8 +1,18 @@
 describe DataProcessor do
   let(:processor) { described_class.new }
 
-  it 'adds processed to valid data' do
-    validator = double(:validator, valid?: true)
-    expect(processor.process('foo', validator)).to eq('foo processed')
+  context 'with valid data' do
+    it 'adds processed to valid data' do
+      validator = double(:validator, valid?: true)
+      expect(processor.process('foo', validator)).to eq('foo processed')
+    end
+  end
+
+  context 'with invalid data' do
+    it 'raises Error' do
+      validator = double(:validator, valid?: false)
+      expect { processor.process('foo', validator) }
+        .to raise_error(DataProcessor::Error)
+    end
   end
 end

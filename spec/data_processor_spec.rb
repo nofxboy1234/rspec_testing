2444b05e536ae7c393602682d223b442523931dc
Dylan Palmboom <nofxboy1234@gmail.com>
2023-05-05 13:09:18 +0200

    Add describe block

--- spec/data_processor_spec.rb
+++ spec/data_processor_spec.rb
@@ -1,31 +1,33 @@
 describe DataProcessor do
   let(:processor) { described_class.new }
 
-  it 'calls validator.valid?' do
-    validator = double(:validator)
-    expect(validator).to receive(:valid?).with('foo').and_return(true)
-    
-    processor.process('foo', validator)
-  end
-
-  context 'with valid data' do
-    it 'adds processed to valid data' do
-      # validator = double(:validator, valid?: true)
+  describe '#process' do
+    it 'calls validator.valid?' do
       validator = double(:validator)
-      allow(validator).to receive(:valid?).and_return(true)
+      expect(validator).to receive(:valid?).with('foo').and_return(true)
 
-      expect(processor.process('foo', validator)).to eq('foo processed')
+      processor.process('foo', validator)
     end
-  end
 
-  context 'with invalid data' do
-    it 'raises Error' do
-      # validator = double(:validator, valid?: false)
-      validator = double(:validator)
-      allow(validator).to receive(:valid?).and_return(false)
+    context 'with valid data' do
+      it 'adds processed to valid data' do
+        # validator = double(:validator, valid?: true)
+        validator = double(:validator)
+        allow(validator).to receive(:valid?).and_return(true)
 
-      expect { processor.process('foo', validator) }
-        .to raise_error(DataProcessor::Error)
+        expect(processor.process('foo', validator)).to eq('foo processed')
+      end
+    end
+
+    context 'with invalid data' do
+      it 'raises Error' do
+        # validator = double(:validator, valid?: false)
+        validator = double(:validator)
+        allow(validator).to receive(:valid?).and_return(false)
+
+        expect { processor.process('foo', validator) }
+          .to raise_error(DataProcessor::Error)
+      end
     end
   end
 end
